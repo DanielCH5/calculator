319f60ba187aacf67a26bbc66a5a29161a371968
DanielCH5 <163397248+DanielCH5@users.noreply.github.com>
2025-05-30 00:16:08 +0200

    Made buttons and display for calculator in HTML

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
-function add (a, b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a, b){
-    return a - b;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a, b){
-    return a / b;
+function divide(a, b) {
+  return a / b;
 }
 
 let firstNumber;
 let operator;
 let secondNumber;
 
+function operate(firstNumber, operator, secondNumber) {
+  switch (operator) {
+    case add:
+      console.log(add(firstNumber, secondNumber));
+      break;
 
-function operate(firstNumber, operator, secondNumber){
-    switch (operator){
-        case add:
-            console.log(
-                add(firstNumber, secondNumber)
-            );
-            break;
+    case subtract:
+      console.log(subtract(firstNumber, secondNumber));
+      break;
 
-        case subtract:
-            console.log(
-                subtract(firstNumber, secondNumber)
-            );
-            break;
+    case multiply:
+      console.log(multiply(firstNumber, secondNumber));
+      break;
 
-        case multiply:
-            console.log(
-                multiply(firstNumber,secondNumber)
-            );
-            break;
-        
-        case divide:
-            console.log(
-                divide(firstNumber,secondNumber)
-            );
-            break;
-    }
+    case divide:
+      console.log(divide(firstNumber, secondNumber));
+      break;
 
-
+    default:
+      console.log("Please enter the values");
+      break;
+  }
 }
