cb27457f7ac4ccd9cff865105f4e8c4518d8bd27
DanielCH5 <163397248+DanielCH5@users.noreply.github.com>
2025-05-29 23:53:22 +0200

    Added 3 variables, and the operate function

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+
+function operate(firstNumber, operator, secondNumber){
+    switch (operator){
+        case add:
+            console.log(
+                add(firstNumber, secondNumber)
+            );
+            break;
+
+        case subtract:
+            console.log(
+                subtract(firstNumber, secondNumber)
+            );
+            break;
+
+        case multiply:
+            console.log(
+                multiply(firstNumber,secondNumber)
+            );
+            break;
+        
+        case divide:
+            console.log(
+                divide(firstNumber,secondNumber)
+            );
+            break;
+    }
+
+
+}
